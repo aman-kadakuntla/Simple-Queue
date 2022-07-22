eb0bb9cd23588f57fe52ef68c14da87d3808133c
Aman11 <kadakuntlaaman@gmail.com>
2022-07-22 10:37:13 +0530

    initial commit

---
+++ Simple Queue Implementation/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Queue_Implementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleQueue simpleQueue = new SimpleQueue(3);
+            label:
+            Console.WriteLine("1. Add \n2. Delete \n3. Check if full\n4. Check if empty\n" +
+                "5. Print front element\n6. Display\n7. Exit");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter an element");
+                    if (simpleQueue.Add(Convert.ToInt32(Console.ReadLine())))
+                        Console.WriteLine("Added successfully");
+                    else
+                        Console.WriteLine("Element cannot be added since queue might be full");
+                    goto label;
+                case 2:
+                    if (simpleQueue.Delete())
+                        Console.WriteLine("Deleted successfully");
+                    else
+                        Console.WriteLine("Queue may be empty");
+                    goto label;
+                case 3:
+                    if (simpleQueue.IsFull())
+                        Console.WriteLine("Queue is full");
+                    else
+                        Console.WriteLine("Queue is not full");
+                    goto label;
+                case 4:
+                    if (simpleQueue.IsEmpty())
+                        Console.WriteLine("Queue is empty");
+                    else
+                        Console.WriteLine("Queue is not empty");
+                    goto label;
+                case 5:
+                    var v=simpleQueue.Peek();
+                    if (v.Item1)
+                        Console.WriteLine("First element in the queue is "+v.Item2);
+                    goto label;
+                case 6:
+                    if(simpleQueue.Display().Count()>0)
+                    {
+                        Console.Write("Elements in the queue are:");
+                        foreach(int i in simpleQueue.Display())
+                        {
+                            Console.Write(i + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Queue is empty");
+                    }
+                    goto label;
+                case 7:
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    goto label;
+            }
+            
+        }
+    }
+}
---
+++ Simple Queue Implementation/SimpleQueue.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Queue_Implementation
+{
+    internal class SimpleQueue
+    {
+        int[] arr;
+        int front, rear;
+        int maxSize;
+        public SimpleQueue(int maxSize)
+        {
+            arr = new int[maxSize];
+            front = -1;
+            rear = -1;
+            this.maxSize = maxSize;
+        }
+
+        public bool Add(int val)
+        {
+            if (rear == maxSize - 1)
+                return false;
+            else if(front==-1 && rear==-1)
+            {
+                front++;
+                rear++;
+                arr[front] = val;
+            }
+            else
+            {
+                rear++;
+                arr[rear] = val;
+            }
+            return true;
+        }
+
+        public bool Delete()
+        {
+            if (front == -1 || front == maxSize)
+                return false;
+            else if (front == maxSize && rear == maxSize)
+            {
+                front = -1; rear = -1;
+            }
+            else
+                front++;
+            return true;
+        }
+        public bool IsFull()
+        {
+            if (front == maxSize - 1 || rear == maxSize - 1)
+                return true;
+            else
+                return false;
+        }
+        public bool IsEmpty()
+        {
+            if (front == -1 || rear == -1||front==maxSize)
+                return true;
+            else
+                return false;
+        }
+        public (bool,int) Peek()
+        {
+            if (front == -1||front==maxSize)
+                return (false,0); 
+            else
+                return (true, arr[front]);
+        }
+        public List<int> Display()
+        {
+            List<int> list = new List<int>();
+            for (int i = front; i <= rear; i++)
+            {
+                list.Add(arr[i]);
+            }
+            return list;
+        }
+    }
+}
